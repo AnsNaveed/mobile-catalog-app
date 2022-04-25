6e1275eceb08230815999bea04ad0176d3406286
ali ali <mianans55@gmail.com>
2022-04-26 03:06:48 +0500

    day 1 complete

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mobile_catalog_app/login_page.dart';
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
+
+      // initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
+    );
+  }
+}
